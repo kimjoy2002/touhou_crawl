75fda499fdb35faa9387995bee426bb816a7d0d5
kimjoy2002 <kimjoy2002@naver.com>
2017-11-05 01:58:27 +0900

    사일런스, 바이올렛필드 좀 더 어둡게

--- th_crawl/display.cpp
+++ th_crawl/display.cpp
@@ -1391,9 +1391,9 @@ void display_manager::game_draw(LPD3DXSPRITE pSprite, ID3DXFont* pfont)
 						env[current_level].dgtile[i+x_][j+y_].draw(pSprite,i*32.0f+20.0f,j*32.0f+20.0f,D3DCOLOR_XRGB(128,128,128),you.turn);
 
 					if(env[current_level].isInSight(coord_def(i+x_,j+y_)) && env[current_level].dgtile[i+x_][j+y_].flag & FLAG_SILENCE)
-						img_effect_slience.draw(pSprite,i*32.0f+20.0f,j*32.0f+20.0f,D3DCOLOR_ARGB(128,0,255,255));
+						img_effect_slience.draw(pSprite,i*32.0f+20.0f,j*32.0f+20.0f,D3DCOLOR_ARGB(80,0,255,255));
 					if(env[current_level].isInSight(coord_def(i+x_,j+y_)) && env[current_level].dgtile[i+x_][j+y_].flag & FLAG_VIOLET)
-						img_effect_slience.draw(pSprite,i*32.0f+20.0f,j*32.0f+20.0f,D3DCOLOR_ARGB(128,255,128,255));
+						img_effect_slience.draw(pSprite,i*32.0f+20.0f,j*32.0f+20.0f,D3DCOLOR_ARGB(80,255,128,255));
 				}
 			}
 		}

fc72254f46b4784e05af5ab23d4d96613c70195b
kimjoy2002 <kimjoy2002@naver.com>
2025-04-10 01:16:30 +0900

    add spell enum

--- th_crawl/spell.cpp
+++ th_crawl/spell.cpp
@@ -410,307 +410,307 @@ string SpellString(spell_list skill)
 	switch(skill)
 	{
 	case SPL_MON_TANMAC_SMALL:
-		return "소형 탄막";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MON_TANMAC_SMALL);
 	case SPL_MON_TANMAC_MIDDLE:
-		return "중형 탄막";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MON_TANMAC_MIDDLE);
 	case SPL_MON_WATER_GUN:
-		return "물총";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MON_WATER_GUN);
 	case SPL_FROST:
-		return "냉기의 탄막";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FROST);
 	case SPL_MEDICINE_CLOUD:
-		return "스위트 포이즌";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MEDICINE_CLOUD);
 	case SPL_COLD_BEAM:
-		return "냉동빔";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_COLD_BEAM);
 	case SPL_CONFUSE:
-		return "혼란";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_CONFUSE);
 	case SPL_SLOW:
-		return "감속";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SLOW);
 	case SPL_SMITE:
-		return "강타";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SMITE);
 	case SPL_FLAME:
-		return "화염의 탄막";	
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FLAME);	
 	case SPL_SUMMON_BUG:
-		return "곤충 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_BUG);
 	case SPL_SELF_HEAL:
-		return "소회복";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SELF_HEAL);
 	case SPL_BLINK:
-		return "블링크";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_BLINK);
 	case SPL_BURN:
-		return "불꽃";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_BURN);
 	case SPL_FROZEN:
-		return "냉기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FROZEN);
 	case SPL_FIRE_WALL:
-		return "화염기둥";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FIRE_WALL);
 	case SPL_FREEZE:
-		return "급속 냉동";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FREEZE);
 	case SPL_STING:
-		return "독의 탄막";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_STING);
 	case SPL_CURE_POISON:
-		return "독치료";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_CURE_POISON);
 	case SPL_TWIST:
-		return "트위스트";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_TWIST);
 	case SPL_SUMMON_MOOK:
-		return "모옥 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_MOOK);
 	case SPL_FIRE_BRAND:
-		return "화염의 무기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FIRE_BRAND);
 	case SPL_COLD_BRAND:
-		return "냉기의 무기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_COLD_BRAND);
 	case SPL_POISON_BRAND:	
-		return "맹독의 무기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_POISON_BRAND);
 	case SPL_SHOCK:
-		return "전격";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SHOCK);
 	case SPL_CHAIN_LIGHTNING:
-		return "체인 라이트닝";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_CHAIN_LIGHTNING);
 	case SPL_DISCHARGE:
-		return "방전";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_DISCHARGE);
 	case SPL_LEVITATION:
-		return "자기부상";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_LEVITATION);
 	case SPL_GLOW:
-		return "조명";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_GLOW);
 	case SPL_GRAZE:
-		return "그레이즈";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_GRAZE);
 	case SPL_VEILING:
-		return "베일링";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_VEILING);
 	case SPL_INVISIBLE:
-		return "투명";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_INVISIBLE);
 	case SPL_HASTE:
-		return "아드레날린";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_HASTE);
 	case SPL_SILENCE:
-		return "정적";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SILENCE);
 	case SPL_SWIFT:
-		return "신속";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SWIFT);
 	case SPL_MAGIC_TANMAC:
-		return "마법의 탄막";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MAGIC_TANMAC);
 	case SPL_FIRE_BALL:
-		return "화염구";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FIRE_BALL);
 	case SPL_FIRE_BOLT:
-		return "파이어볼트";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FIRE_BOLT);
 	case SPL_ICE_BOLT:
-		return "아이스볼트";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_ICE_BOLT);
 	case SPL_VENOM_BOLT:
-		return "베놈볼트";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_VENOM_BOLT);
 	case SPL_CONFUSE_CLOUD:
-		return "악취의 구름";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_CONFUSE_CLOUD);
 	case SPL_POISON_CLOUD:
-		return "독의 구름";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_POISON_CLOUD);
 	case SPL_ICE_CLOUD:
-		return "냉기의 구름";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_ICE_CLOUD);
 	case SPL_SUMMON_BIRD:
-		return "새 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_BIRD);
 	case SPL_RECALL:
-		return "리콜";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_RECALL);
 	case SPL_TELEPORT_OTHER:
-		return "목표의 공간이동";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_TELEPORT_OTHER);
 	case SPL_TELEPORT_SELF:
-		return "자신의 공간이동";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_TELEPORT_SELF);
 	case SPL_WHIRLWIND:
-		return "풍선풍";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_WHIRLWIND);
 	case SPL_SUMMON_PENDULUM:
-		return "펜듈럼 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_PENDULUM);
 	case SPL_SUMMON_SEKIBANKI:
-		return "머리 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_SEKIBANKI);
 	case SPL_WATER_CANNON:
-		return "워터캐논";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_WATER_CANNON);
 	case SPL_KYOKO_SMITE:
-		return "극대 메아리";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_KYOKO_SMITE);
 	case SPL_SUMMON_OPTION:
-		return "옵션 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_OPTION);
 	case SPL_SUMMON_GOLEM:
-		return "골렘 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_GOLEM);
 	case SPL_HYPNOSIS:
-		return "최면";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_HYPNOSIS);
 	case SPL_MUTE:
-		return "실어증";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MUTE);
 	case SPL_SELF_INJURY:
-		return "자해";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SELF_INJURY);
 	case SPL_CHARM:
-		return "매료";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_CHARM);
 	case SPL_LASER:
-		return "레이저";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_LASER);
 	case SPL_SPARK:
-		return "스파크";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SPARK);
 	case SPL_SUMMON_UNZAN:
-		return "운잔 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_UNZAN);
 	case SPL_SUMMON_UNZAN_PUNCH:
-		return "운잔 철권";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_UNZAN_PUNCH);
 	case SPL_SUMMON_ZOMBIE_FAIRY:
-		return "좀비페어리 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_ZOMBIE_FAIRY);
 	case SPL_SUMMON_UFO:
-		return "벤토라 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_UFO);
 	case SPL_HASTE_OTHER:
-		return "아군 가속";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_HASTE_OTHER);
 	case SPL_HEAL_OTHER:
-		return "아군 회복";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_HEAL_OTHER);
 	case SPL_MIND_BENDING:
-		return "마인드 벤딩";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MIND_BENDING);
 	case SPL_STONE_PUNCH:
-		return "돌 주먹";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_STONE_PUNCH);
 	case SPL_STONE_ARROW: 
-		return "바위의 탄막";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_STONE_ARROW);
 	case SPL_STONE_TRAP:
-		return "압정 바위";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_STONE_TRAP);
 	case SPL_STONE_UPLIFT:
-		return "지반 융기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_STONE_UPLIFT);
 	case SPL_KANAME_DRILL:
-		return "카나메 드릴";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_KANAME_DRILL);
 	case SPL_DIAMOND_HARDNESS:
-		return "다이아몬드 하드니스";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_DIAMOND_HARDNESS);
 	case SPL_POISON_SKIN:
-		return "포이즌 바디";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_POISON_SKIN);
 	case SPL_STONE_FORM:
-		return "무념무상의 경지";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_STONE_FORM);
 	case SPL_KNIFE_COLLECT:
-		return "탄막 회수";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_KNIFE_COLLECT);
 	case SPL_FLAN_BUSIN:
-		return "포 오브 어카인드";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FLAN_BUSIN);
 	case SPL_BURST:
-		return "폭파";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_BURST);
 	case SPL_SUICIDE_BOMB:
-		return "자폭";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUICIDE_BOMB);
 	case SPL_RABBIT_HORN:
-		return "죽림의 나팔";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_RABBIT_HORN);
 	case SPL_SUMMON_LESSOR_DEMON:
-		return "마족 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_LESSOR_DEMON);
 	case SPL_LUMINUS_STRIKE:
-		return "루미너스 스트라이크";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_LUMINUS_STRIKE);
 	case SPL_FIRE_STORM:
-		return "화염 폭풍";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FIRE_STORM);
 	case SPL_BLIZZARD: 
-		return "블리자드";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_BLIZZARD);
 	case SPL_PERFERT_FREEZE: 
-		return "퍼펙트 프리즈";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_PERFERT_FREEZE);
 	case SPL_DRAW_POWER:
-		return "상단회수";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_DRAW_POWER);
 	case SPL_ANIMAL_CHANGE:
-		return "강제동물변화";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_ANIMAL_CHANGE);
 	case SPL_FIELD_VIOLET:
-		return "필드울트라바이올렛";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FIELD_VIOLET);
 	case SPL_TIME_PARADOX: 
-		return "타임 패러독스";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_TIME_PARADOX);
 	case SPL_PRIVATE_SQ: 
-		return "프라이빗 스퀘어";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_PRIVATE_SQ);
 	case SPL_CONTROLED_BLINK: 
-		return "제어 순간이동";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_CONTROLED_BLINK);
 	case SPL_THE_WORLD:
-		return "더 월드";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_THE_WORLD);
 	case SPL_HASTE_ALL:
-		return "단체 가속";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_HASTE_ALL);
 	case SPL_HEAL_ALL:
-		return "단체 회복";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_HEAL_ALL);
 	case SPL_MOON_COMMUNICATION:
-		return "전파 수신";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MOON_COMMUNICATION);
 	case SPL_MOON_GUN:
-		return "달의 총";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MOON_GUN);
 	case SPL_SUMMON_DREAM:
-		return "꿈의 소환";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_DREAM);
 	case SPL_MANA_DRAIN:
-		return "영력흡수";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MANA_DRAIN);
 	case SPL_INSANE:
-		return "광기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_INSANE);
 	case SPL_BLOOD_SMITE:
-		return "피의 응시";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_BLOOD_SMITE);
 	case SPL_CALL_HOUND:
-		return "동료부르기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_CALL_HOUND);
 	case SPL_CANNON:
-		return "음양탄";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_CANNON);
 	case SPL_DOLLS_WAR:
-		return "돌즈워";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_DOLLS_WAR);
 	case SPL_FAKE_DOLLS_WAR:
-		return "모조 돌즈워";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FAKE_DOLLS_WAR);
 	case SPL_FIRE_SPREAD:
-		return "화염 분출";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_FIRE_SPREAD);
 	case SPL_STASIS:
-		return "체크메이트";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_STASIS);
 	case SPL_JUMP_ATTACK:
-		return "도약공격";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_JUMP_ATTACK);
 	case SPL_ALERT_NOISE: 
-		return "경고의 메아리";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_ALERT_NOISE);
 	case SPL_SUMMON_NAMAZ:
-		return "나마즈 소환";		
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_NAMAZ);		
 	case SPL_SCHEMA_TANMAC:
-		return "경계 탄막";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SCHEMA_TANMAC);
 	case SPL_CHANGE:
-		return "둔갑술";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_CHANGE);
 	case SPL_UNLUCK:
-		return "파국의 개문";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_UNLUCK);
 	case SPL_THUNDER:
-		return "용신의 번개";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_THUNDER);
 	case SPL_AIR_STRIKE:
-		return "질풍선";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_AIR_STRIKE);
 	case SPL_SUMMON_RACOON:
-		return "너구리 탄막변화";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_RACOON);
 	case SPL_SUMMON_YOUKAI:
-		return "백귀야행";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_YOUKAI);
 	case SPL_MAMIZO_EVADE:
-		return "이승지장변화";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MAMIZO_EVADE);
 	case SPL_MACRO_BURST:
-		return "매크로버스트";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MACRO_BURST);
 	case SPL_SHATTER:
-		return "분쇄";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SHATTER);
 	case SPL_SUMMON_YOSHIKA:
-		return "강령 요시카";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_YOSHIKA);
 	case SPL_NESY_CANNON:
-		return "네시 물대포";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_NESY_CANNON);
 	case SPL_MERMAID_SONG:
-		return "인어의 노래";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MERMAID_SONG);
 	case SPL_EMERALD_CITY:
-		return "에메랄드 시티";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_EMERALD_CITY);
 	case SPL_AUTUMN_BLADE:
-		return "어텀 블레이드";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_AUTUMN_BLADE);
 	case SPL_PHILOSOPHERS_STONE:
-		return "현자의 돌";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_PHILOSOPHERS_STONE);
 	case SPL_SUMMON_ANCHOR:
-		return "침몰 앵커";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_ANCHOR);
 	case SPL_REAPER_MET:
-		return "사신의마중 체험판";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_REAPER_MET);
 	case SPL_AFTERLITE:
-		return "얼마없는 여생";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_AFTERLITE);
 	case SPL_PRISM_CALL:
-		return "프리즘 콘체르토";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_PRISM_CALL);
 	case SPL_PSYCHOKINESIS:
-		return "텔레키네시스";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_PSYCHOKINESIS);
 	case SPL_SUMMON_TRASH:
-		return "어번 사이코키네시스";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_TRASH);
 	case SPL_TRASH_RUSH:
-		return "어번 사이코키네시스 사출";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_TRASH_RUSH);
 	case SPL_KOKORO_CHANGE:
-		return "코코로 룰렛";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_KOKORO_CHANGE);
 	case SPL_THUNDER_BOLT:
-		return "썬더볼트";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_THUNDER_BOLT);
 	case SPL_SANTUARY:
-		return "성역";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SANTUARY);
 	case SPL_MISTIA_SONG:
-		return "밤참새의 노래";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MISTIA_SONG);
 	case SPL_THROW_DISH:
-		return "접시던지기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_THROW_DISH);
 	case SPL_MESS_CONFUSION:
-		return "광역 혼란";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_MESS_CONFUSION);
 	case SPL_SLEEP_SMITE:
-		return "꿈으로 인도";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SLEEP_SMITE);
 	case SPL_TARGET_ELEC:
-		return "전기 충격";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_TARGET_ELEC);
 	case SPL_SUMMON_ELEC_BALL:
-		return "용궁의사자 유영탄";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_SUMMON_ELEC_BALL);
 	case SPL_DREAM_CALL:
-		return "드림캐쳐";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_DREAM_CALL);
 	case SPL_HYPER_BEAM:
-		return "파괴광선";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_HYPER_BEAM);
 	case SPL_KAGUYA_SPELL:
-		return "5개의 난제";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_KAGUYA_SPELL);
 	case SPL_THROW_SWORD:
-		return "검 던지기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_THROW_SWORD);
 	case SPL_THROW_KNIFE:
-		return "나이프 던지기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_THROW_KNIFE);
 	case SPL_THROW_PLAYER:
-		return "천수력남 던지기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_THROW_PLAYER);
 	case SPL_THROW_AMULET:
-		return "부적 던지기";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_THROW_AMULET);
 	case SPL_WARP_KICK:
-		return "아공혈";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_WARP_KICK);
 	case SPL_REIMU_BARRIER:
-		return "봉마진";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_REIMU_BARRIER);
 	default:
-		return "알수없는 마법";
+		return LocalzationManager::locString(LOC_SYSTEM_SPL_UKNOWN);
 	}
 }
 int SpellLevel(spell_list skill)
